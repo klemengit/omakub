01cd5f0a88201a81d7faba49fc476f523886f960
Klemen Zaletelj <klemen.zaletelj@fs.uni-lj.si>
2025-11-12 06:04:41 +0100

    removed auto texlive install

--- install/terminal/app-texlive.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install LaTeX packages
-gum spin --spinner meter --title "Installing LaTeX (this may take a while)..." -- \
-  sudo apt install -y \
-  texlive-full \
-  latexmk
-
-echo "✓ LaTeX installation complete"
